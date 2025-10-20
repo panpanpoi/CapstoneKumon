4b4ee39b1e86da29a47a91a5b7f57d320e5a55a2
panpanpoi <lukeelreyes@gmail.com>
2025-10-21 00:17:31 +0800

    online database

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'sql306.infinityfree.com';     // ✅ Your MySQL Hostname
-$dbname = 'if0_40208721_kumon_ortigas_dbX';          // ✅ Replace XXX with your actual database name (shown below that section)
+$dbname = 'if0_40208721_kumon_ortigas_db'; // ✅ Replace XXX with your actual database name (shown below that section)
 $username = 'if0_40208721';            // ✅ Your MySQL Username
 $password = '7af9Ei6qmr';        // ✅ The password you created in InfinityFree
 
