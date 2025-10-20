542ff1688b2eb241310a0b264fdff5b64e45f9b7
panpanpoi <lukeelreyes@gmail.com>
2025-10-21 00:09:38 +0800

    database online

--- database.php
+++ database.php
@@ -1,17 +1,13 @@
 <?php
+$host = 'sql306.infinityfree.com';     // ✅ Your MySQL Hostname
+$dbname = 'if0_40208721_kumon_ortigas_dbX';          // ✅ Replace XXX with your actual database name (shown below that section)
+$username = 'if0_40208721';            // ✅ Your MySQL Username
+$password = '7af9Ei6qmr';        // ✅ The password you created in InfinityFree
 
-$dns = "mysql:host=localhost;dbname=kumon_ortigas_db";
-$dbuser = "root";
-$dbpass = "";
-
-//error handling
 try {
-    $pdo = new PDO($dns, $dbuser, $dbpass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die('Database connection failed: ' . $e->getMessage());
+    die("Database connection failed: " . $e->getMessage());
 }
-
+?>
